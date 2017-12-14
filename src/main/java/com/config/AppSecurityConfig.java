3dd33711d4b13566ae88e39a57270c2c2345722a
Neonet39 <wolf1syn>
2017-12-15 02:15:17 +0300

    bag commit

--- src/main/java/com/config/AppSecurityConfig.java
+++ src/main/java/com/config/AppSecurityConfig.java
@@ -1,7 +1,11 @@
 package com.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.security.test.TokenAuthenticationEntryPoint;
+import com.security.test.TokenAuthenticationFilter;
+import com.security.test.TokenAuthenticationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,38 +16,52 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 @ComponentScan(basePackages = {"com.security"})
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    @Qualifier("customAuthenticationProvider")
-    AuthenticationProvider authenticationProvider;
+
+
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
-        // auth.authenticationProvider(preAuthenticatedAuthenticationProvider());
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("adminPassword").roles("USER", "ADMIN").and()
+                .and()
+                .authenticationProvider(tokenAuthenticationProvider(auth.getDefaultUserDetailsService()));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http
-                .formLogin()
-                 .loginPage("/login")
-                .permitAll()
+        http.addFilterBefore(new TokenAuthenticationFilter(authenticationManager(), tokenAuthenticationEntryPoint(), "X-Auth-Token", false), BasicAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                // .antMatchers("/mainPage").authenticated()
-                .antMatchers("/mainPage").access("hasRole('USER')")
-                .anyRequest().authenticated();
-
-
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic().and()
+                .formLogin().and()
+                .csrf().disable();
     }
 
+    @Bean
+    public AuthenticationEntryPoint tokenAuthenticationEntryPoint() {
+        return new TokenAuthenticationEntryPoint();
+    }
 
+    @Bean
+    public AuthenticationProvider tokenAuthenticationProvider(UserDetailsService userDetailsService) {
+
+        return new TokenAuthenticationProvider(userDetailsService);
+    }
 
 }
